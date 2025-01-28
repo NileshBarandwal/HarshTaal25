c164fcef68f5aacb5a5a9a788bc8759c609433b3
Priyanshu Rao <140475711+PriyanshuRao-code@users.noreply.github.com>
2025-01-28 10:45:48 +0530

    Contact Us changed with icon hover effect left. Background colors changed to logo matching colors

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import '../styles/Contact.css';
+import React from "react";
+import "../styles/Contact.css";
+import "remixicon/fonts/remixicon.css";
 
 const Contact = () => (
   <section id="contact" className="contact">
-    <h2>Contact Us</h2>
-    <div className="social-links">
-      <a href="mailto:contact@harshtal.com">Email</a>
-      <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
-      <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>
+    <div class="container">
+      <h2>Contact Us</h2>
+      <div className="social-links">
+        <a href="mailto:contact@harshtal.com">
+          <i className="ri-mail-line logos"></i>
+        </a>
+        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+          <i className="ri-instagram-line logos"></i>
+        </a>
+        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+          <i className="ri-facebook-circle-line logos"></i>
+        </a>
+      </div>
     </div>
   </section>
 );
