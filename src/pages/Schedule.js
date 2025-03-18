19a48f69dac707a122c51cbbc6f9a4eb23654a0e
sreehb-123 <sreeh190206@gmail.com>
2025-03-19 02:39:10 +0530

    download option for schedule

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -2,13 +2,31 @@ import '../styles/Schedule.css';
 import HomeNav from '../components/HomeNav/HomeNav.js';
 
 const Timeline = () => {
+
+    const handleDownload = () => {
+        const link = document.createElement('a');
+        link.href = '/HarshTaal_2.0_timeline_cropped.jpg';
+        link.download = 'HarshTaal_2.0_timeline_cropped.jpg';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
     return (
         <div className='schedule-container'>
             <HomeNav />
             <h1>Schedule</h1>
-            <img src="/HarshTaal_2.0_timeline_cropped.jpg" alt='harshtal-schedule' className='schedule-img'></img>
+            <p>Click on the image to download</p>
+
+            <img 
+                src="/HarshTaal_2.0_timeline_cropped.jpg" 
+                alt='harshtal-schedule' 
+                className='schedule-img'
+                onClick={handleDownload}
+                style={{ cursor: 'pointer' }}
+            />
         </div>
-    )
+    );
 };
 
 export default Timeline;
